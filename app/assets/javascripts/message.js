494c9c323f2d46bad6c8f12c465c32ae64c79b4a
RyoAsai-git <61679701+RyoAsai-git@users.noreply.github.com>
2020-03-10 14:04:16 +0900

    asynchronous

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,61 @@
+$(function(){
+    function buildHTML(message){
+      if ( message.image ) {
+        var html =
+        `<div class="message">
+            <div class="upper-message">
+              <div class="upper-message__user-name">
+                ${message.user_name}
+              </div>
+              <div class="upper-message__date">
+                ${message.created_at}
+              </div>
+            </div>
+            <div class="lower-message">
+              ${message.body}
+                <img src=${message.image} ></img>
+            </div>
+        </div>`
+      return html;
+   } else {
+        var html = 
+        `<div class="message">
+            <div class="upper-message">
+              <div class="upper-message__user-name">
+                ${message.user_name}
+              </div>
+              <div class="upper-message__date">
+                ${message.created_at}
+              </div>
+            </div>
+            <div class="lower-message">
+              ${message.body}
+            </div>
+         </div>`
+      return html;
+      };
+    }
+$('#new_message').on('submit', function(e){
+    e.preventDefault()
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('form')[0].reset();
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      $('.ul-submit').prop('disabled', false);
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+  });
+})
+});
